93a8fd3b5c05262fdae44b60bc5c6d33bb1419ee
Adam-zrigui <zriguiadam@gmail.com>
2026-03-02 22:54:15 +0100

    ..

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://yourdomain.com',
+  siteUrl: process.env.SITE_URL || 'https://smart-budget-delta.vercel.app',
   generateRobotsTxt: false, // we provide our own
   changefreq: 'daily',
   priority: 0.7,
